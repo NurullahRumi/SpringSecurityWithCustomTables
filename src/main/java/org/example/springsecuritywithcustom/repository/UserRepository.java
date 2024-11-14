aaf3ceb33ef5f16b7a35736ac541d9bc3a7053e9
NurullahRumi <nurullahrumi@gmail.com>
2024-11-15 03:26:21 +0600

    added user and role repo

---
+++ src/main/java/org/example/springsecuritywithcustom/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package org.example.springsecuritywithcustom.repository;
+
+
+import org.example.springsecuritywithcustom.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+}
